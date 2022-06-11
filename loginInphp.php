906f5b8bd7f8cf8613a48f656418383b4d629251
Given Makofane <93738259+makofane41@users.noreply.github.com>
2022-06-11 20:47:00 +0200

    initial

---
+++ loginInphp.php
@@ -0,0 +1,45 @@
+<?php
+//login feature usind encryption method md5
+//include ur database
+
+//backend code
+if(isset($_POST['login']))
+{
+   $email=$_POST['email'];
+   $password=md5($_POST['password']);
+$query=mysqli_query($con,"SELECT * FROM users WHERE email='$email' and password='$password'");
+$num=mysqli_fetch_array($query);
+if($num>0)
+{
+
+  //session can be deleted but for more info about session email me for content makofane411@gmail.com
+    
+$_SESSION['login']=$_POST['email'];
+$_SESSION['id']=$num['id'];
+$_SESSION['username']=$num['name'];
+$uip=$_SERVER['REMOTE_ADDR'];
+$status=1;
+$log=mysqli_query($con,"insert into userlog(userEmail,userip,status) values('".$_SESSION['login']."','$uip','$status')");
+$host=$_SERVER['HTTP_HOST'];
+$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+exit();
+}
+else
+{
+
+$email=$_POST['email'];
+$uip=$_SERVER['REMOTE_ADDR'];
+$status=0;
+$log=mysqli_query($con,"insert into userlog(userEmail,userip,status) values('$email','$uip','$status')");
+$host  = $_SERVER['HTTP_HOST'];
+$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+$_SESSION['errmsg']="Invalid email id or Password";
+exit();
+}
+}
+
+
+?>
+
+
+
---
+++ registerAccount.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+error_reporting(0);
+//include ur database
+
+
+// backend Code user Registration
+//for more content email makofane411@gmail.com
+
+if(isset($_POST['submit']))
+{
+$name=$_POST['fullname'];
+$email=$_POST['emailid'];
+$contactno=$_POST['contactno'];
+$password=md5($_POST['password']);
+$query=mysqli_query($con,"insert into users(name,email,contactno,password) values('$name','$email','$contactno','$password')");
+if($query)
+{
+	echo "<script>alert('You are successfully register');</script>";
+}
+else{
+echo "<script>alert('Not register something went worng');</script>";
+}
+}
+?>
